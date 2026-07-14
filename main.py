8cbb7a7295c918caf715257db4a9a8a3bf6a3ab9
SunJong2 <tjsqhd9790@gmail.com>
2026-07-14 18:45:10 +0900

    로그인 화면 구현: 디자인 시스템, localStorage 토큰 저장

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from database import init_db
 from auth import router as auth_router
 from goals import router as goals_router
 from savings import router as savings_router
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
@@ -11,6 +12,7 @@ init_db()
 app.include_router(auth_router)
 app.include_router(goals_router)
 app.include_router(savings_router)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 def root():
